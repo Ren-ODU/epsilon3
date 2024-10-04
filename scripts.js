ec4c7e9012b8a38d4eb46d0d68002337f4105afa
Ren Macauley <117415376+Ren-ODU@users.noreply.github.com>
2024-10-04 12:33:23 -0400

    Create scripts.js
    scripts.js as it was created in W3Schools Spaces (moved due to exceeding free plan). Currently contains a commented-out initial attempt at an animation using Font Awesome icons. To be updated.

---
+++ scripts.js
@@ -0,0 +1,25 @@
+/* Place your JavaScript in this file */
+
+/*********************** Animated Icon ***********************/
+<script>
+function earthspin() {
+  /*
+  div {
+  width: 50px;
+  height: 50px;
+  background-color: red;
+  position: relative;
+  animation-name: earthSpin;
+  animation-duration: 1s;
+   animation-iteration-count: infinite;
+}
+
+@keyframes earthSpin {
+  0%   {<i class="fa-solid fa-earth-oceania"></i>}
+  25%  {<i class="fa-solid fa-earth-europe"></i>}
+  50%  {<i class="fa-solid fa-earth-asia"></i>}
+  75%  {<i class="fa-solid fa-earth-africa"></i>}
+  100% {<i class="fa-solid fa-earth-oceania"></i>}
+}
+  */
+</script>
