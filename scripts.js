fee0c0ba3720ce41824c2d914c8c555781c36960
Ren Macauley <117415376+Ren-ODU@users.noreply.github.com>
2024-10-05 16:02:10 -0400

    big revert scripts.js

--- scripts.js
+++ scripts.js
@@ -1,31 +1,29 @@
 /* Place your JavaScript in this file */
 
 /*********************** Slideshow ***********************/
-let slideIndex = [1,1];
-let slideId = ["mySlides1", "mySlides2"]
-showSlides(1, 0);
-showSlides(1, 1);
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function currentSlide(n, no) {
-  showSlides(slideIndex[no] = n, no);
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-function showSlides(n, no) {
+function showSlides(n) {
   let i;
-  let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
-
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  x[slideIndex[no]-1].style.display = "block";  
+  slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
