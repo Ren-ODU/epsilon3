46403599d0cc1ccdbc4ec4e619f48ee3042fe9dc
Ren Macauley <117415376+Ren-ODU@users.noreply.github.com>
2024-10-05 13:10:55 -0400

    Update scripts.js - Change slideshow type, remove commented out code

--- scripts.js
+++ scripts.js
@@ -4,12 +4,10 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -17,40 +15,15 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
 }
-
-/*********************** Animated Icon ***********************/
-/*
-function earthspin() {
-  
-  div {
-  width: 50px;
-  height: 50px;
-  background-color: red;
-  position: relative;
-  animation-name: earthSpin;
-  animation-duration: 1s;
-   animation-iteration-count: infinite;
-}
-
-@keyframes earthSpin {
-  0%   {<i class="fa-solid fa-earth-oceania"></i>}
-  25%  {<i class="fa-solid fa-earth-europe"></i>}
-  50%  {<i class="fa-solid fa-earth-asia"></i>}
-  75%  {<i class="fa-solid fa-earth-africa"></i>}
-  100% {<i class="fa-solid fa-earth-oceania"></i>}
-}
-*/
